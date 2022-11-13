c30dee62ca9592552b8f5a73793c95e769119a19
SaumyaSrivastava <saumya2113061@akgec.ac.in>
2022-11-13 12:22:43 +0530

    auth file has been added

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const app = express();
 
@@ -15,8 +16,15 @@ app.use(
   })
 );
 app.use(express.static("public"));
+app.use(cookieParser());
 
-//TODO
+app.get("/logout", auth, async (req, res) => {
+  try {
+    console.log("logout successfully");
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
