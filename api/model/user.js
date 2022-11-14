76b2b13af68f0c67676a5442bac8394788974ac6
SaumyaSrivastava <saumya2113061@akgec.ac.in>
2022-11-14 16:00:07 +0530

    login added

--- api/model/user.js
+++ api/model/user.js
@@ -2,25 +2,10 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
 
-  username: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    index: {
-      unique: true,
-    },
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  data: {
-    type: Date,
-    default: Date.now,
-  },
+  username: String,
+  password: String,
+  phone: Number,
+  email: String,
 });
 
 module.exports = mongoose.model("user", userSchema);
